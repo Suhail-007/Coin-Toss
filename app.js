a38cc4903a4af409a45ad7c1971879b60dc6019b
Suhail-007 <suhuq007@gmail.com>
2021-12-21 14:45:53 +0530

    Fixed Animation lag, Fixed score not adding

--- app.js
+++ app.js
@@ -1,88 +1,81 @@
-let buttons = document.querySelector('[data-btns-div]');
-
-let playerOutput = document.getElementById('player');
-let compOutput = document.querySelector('#comp');
-
+let buttonsDiv = document.querySelector('[data-btns-div]');
 let userScore = 0;
 let computerScore = 0;
 let heads = 1;
 let tails = 0;
 
-let p = document.createElement('p');
- 
- 
-//this will change according to the random number
-function headsAndTails(random) {
-let showHeads = document.querySelector('.heads');
-
-		if (random === 1) {
-				showHeads.textContent = 'Heads';
-				showHeads.style.color = 'red';
+buttonsDiv.addEventListener('click', (event) => {
+	let userSelection;
+	let computerSelection;
+//this will generate a random number everytime	
+	let randomNumber = Math.round(Math.random());
+	
+		if (event.target.dataset.head) {
+				userSelection = heads;
+				computerSelection = tails;
+		} else if (event.target.dataset.tail) {
+				userSelection = tails;
+				computerSelection = heads;
 		}
 		
-		if (random === 0) {
-				showHeads.textContent = 'Tails';
-				showHeads.style.color = '';
-		}
-}
+setTimeout(() => {
+addScore(randomNumber, userSelection, computerSelection)}, 500);
 
-//random number either be 1 or 0
-function score(random, userChoose, computerChoose) {
-		//calling this function here so it won't get delay showing the heads and tails
-		headsAndTails(random);
-		if (random === userChoose) {
-				playerOutput.style.color = 'green';
-				playerOutput.style.fontWeight = 'bold';
-				compOutput.style.fontWeight = '';
-			 userScore++;
+animate();
+});
+
+function addScore(randomNum, userPick, comPick) {
+isHeadTail(randomNum)
+	let playerOutput = document.querySelector('#player');
+	let compOutput = document.querySelector('#comp');
+	//randomNum which will be either 0 or 1
+		if (userPick === randomNum) {
+				userScore++
 				playerOutput.textContent = `Player: ${userScore}`;
-				compOutput.style.color = 'red';
-		} else if (random === computerChoose) {
-				compOutput.style.color = 'green';
+				playerOutput.style.fontWeight = 'bold';
+				playerOutput.style.color = 'green';
+				compOutput.style.fontWeight = '';
+			 compOutput.style.color = 'red';
+		} else 	if (comPick === randomNum) {
+				computerScore++
+		  	compOutput.textContent = `Computer: ${computerScore}`;
 				compOutput.style.fontWeight = 'bold';
-				playerOutput.style.fontWeight = '';
-				playerOutput.style.color = 'red';
-				computerScore++;
-				compOutput.textContent = `Computer: ${computerScore}`;
+			 compOutput.style.color = 'green';
+			 playerOutput.style.color = 'red';
+				playerOutput.style.fontWeight = '';			 
 		}
-}
+		isWinner()
+};
 
-function winner() {
-		if (userScore === 3) {		
-				p.innerHTML		= `You Won!!`
-		} else if (computerScore === 3) {
-				p.innerHTML = `Computer won!`
-		}
-document.querySelector('.container').append(p);				
-}
+function isHeadTail(randomNum) {
+ let tails = document.querySelector('.tails');
+		if (randomNum === 1) {
+				tails.textContent = 'Heads';
+				tails.style.color = 'red';
+		} else {
+				tails.textContent = 'Tails';
+				tails.style.color = '';
+		}		
+};
 
+function animate() {
+		let tails = document.querySelector('.tails');
+		let heads = document.querySelector('.heads');
+		
+		tails.style.animation = 'upAndRotate 500ms alternate';
+		heads.style.animation = 'upAndRotate 500ms alternate';
+		
+		tails.addEventListener('animationend', () => {
+		tails.style.animation = '';
+		heads.style.animation = '';
+		})
+};
 
-buttons.addEventListener('click', (e) => {
-		let userSelection;
-		let computerSelection;
-					
-		//this will generate a new number everytime we call on click function, math.round will round the number to nearest integer
-	let randomNumber = Math.round(Math.random()); //0 or 1
-		console.log(randomNumber)	
-			
-			if (e.target.dataset.head)	{
-					userSelection = heads;
-					computerSelection = tails;	
-			//1 is heads and 0 is tails line 8 and 9
-			} else if (e.target.dataset.tail) {
-				userSelection = tails;
-				computSelection = heads;			
-			}      
-
- 	let coin = document.querySelector('.coin');
-	coin.classList.add('active');
-	
-   setTimeout(() => {
-   		score(randomNumber, userSelection, computerSelection);
-   		}, 1000);
-              
- setTimeout(() => {
-   coin.classList.remove('active');
-            }, 2000); 
-winner();   			
-		});
+function isWinner() {
+ 		let message = document.getElementById('message');
+		if (userScore === 5) {		
+				message.textContent = 'You Win!!';
+		} else if (computerScore === 5) {
+				message.textContent = 'Computer Win!!'
+		};
+};
